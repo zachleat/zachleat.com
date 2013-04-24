bfabd1d12744ee43915cefadb29f1f837031d0c1
Zach Leatherman <zachleatherman@gmail.com>
2013-04-23 22:17:58 -0500

    Layout cleanup, adding yepnope to load the syntax highlighter.

---
+++ js/initial.js
@@ -0,0 +1,25 @@
+;(function() {
+	var dirs = {
+		root: '/web/'
+	};
+	dirs.lib = dirs.root + 'lib/';
+
+	yepnope.injectJs(dirs.lib + 'prism.js');
+	yepnope.injectCss(dirs.lib + 'prism.css');
+})();
+
+//
+// Google Analytics
+//
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', '']);
+// _gaq.push(['_trackPageview']);
+
+// (function() {
+// 	var ga = document.createElement('script');
+// 	ga.type = 'text/javascript';
+// 	ga.async = true;
+// 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+// 	var s = document.getElementsByTagName('script')[0];
+// 	s.parentNode.insertBefore(ga, s);
+// })();
