21d4eb3ac4abf41b721b036b6e4d55958ab17b5c
Zach Leatherman <zachleatherman@gmail.com>
2013-06-16 12:14:41 -0500

    Minor updates for 0 minutes on reading time.

--- web/_plugins/reading_time.rb
+++ web/_plugins/reading_time.rb
@@ -1,3 +1,9 @@
+# Outputs the reading time
+ 
+# Read this in about “4 minutes”
+# Put into your _plugins dir in your Jekyll site
+# Usage: Read this in {{ page.content | reading_time }}.
+
 module ReadingTimeFilter
 	def reading_time( input )
 		words_per_minute = 180
@@ -5,7 +11,7 @@ module ReadingTimeFilter
 		words = input.split.size;
 		minutes = ( words / words_per_minute ).floor
 		minutes_label = minutes === 1 ? " minute" : " minutes"
-		"#{minutes} #{minutes_label}"
+		minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute"
 	end
 end
 
