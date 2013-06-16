2e55db9eb9acb5c85ca281f090f83d5aa3c33685
Zach Leatherman <zachleatherman@gmail.com>
2013-06-16 12:03:04 -0500

    Underestimate the reading time (not sure why .ceil wasn’t working)

--- web/_plugins/reading_time.rb
+++ web/_plugins/reading_time.rb
@@ -3,7 +3,7 @@ module ReadingTimeFilter
 		words_per_minute = 180
 
 		words = input.split.size;
-		minutes = ( words / words_per_minute ).ceil
+		minutes = ( words / words_per_minute ).floor
 		minutes_label = minutes === 1 ? " minute" : " minutes"
 		"#{minutes} #{minutes_label}"
 	end
