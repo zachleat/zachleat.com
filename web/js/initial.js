e7912017032a731cf6f958c94cacaae35b23a839
Zach Leatherman <zachleatherman@gmail.com>
2015-01-23 01:00:02 -0600

    Fixes WebKit

--- web/js/initial.js
+++ web/js/initial.js
@@ -32,11 +32,10 @@ var ZL = {
 
 	// TODO import this using npm and grunt
 
-	/*! fontfaceonload - v0.1.4 - 2015-01-01
-	 * https://github.com/zachleat/fontfaceonload
-	 * Copyright (c) 2015 Zach Leatherman (@zachleat)
-	 * MIT License */
-	;(function(e,t){"use strict";var n="AxmTYklsjo190QW",r="sans-serif",i="serif",s={tolerance:2,delay:100,glyphs:"",success:function(){},error:function(){},timeout:5e3},o=["display:block","position:absolute","top:-999px","left:-999px","font-size:48px","width:auto","height:auto","line-height:normal","margin:0","padding:0","font-variant:normal","white-space:nowrap"],u='<div style="%s">'+n+"</div>",a=function(){this.fontFamily="",this.appended=!1,this.serif=undefined,this.sansSerif=undefined,this.parent=undefined,this.options={}};a.prototype.getMeasurements=function(){return{sansSerif:{width:this.sansSerif.offsetWidth,height:this.sansSerif.offsetHeight},serif:{width:this.serif.offsetWidth,height:this.serif.offsetHeight}}},a.prototype.load=function(){function g(e,t,n){return Math.abs(e.width-t.offsetWidth)>n||Math.abs(e.height-t.offsetHeight)>n}function y(){return(new Date).getTime()-n.getTime()>p.timeout}var n=new Date,s=this,a=s.serif,f=s.sansSerif,l=s.parent,c=s.appended,h,p=this.options,d=p.reference,v=u.replace(/\%s/,o.concat("font-family:"+r).join(";")),m=u.replace(/\%s/,o.concat("font-family:"+i).join(";"));l||(l=s.parent=t.createElement("div")),l.innerHTML=v+m,f=s.sansSerif=l.firstChild,a=s.serif=f.nextSibling,p.glyphs&&(f.innerHTML+=p.glyphs,a.innerHTML+=p.glyphs),function b(){d||(d=t.body),!c&&d&&(d.appendChild(l),c=s.appended=!0,h=s.getMeasurements(),f.style.fontFamily=s.fontFamily+", "+r,a.style.fontFamily=s.fontFamily+", "+i),c&&h&&(g(h.sansSerif,f,p.tolerance)||g(h.serif,a,p.tolerance))?p.success():y()?p.error():!c&&"requestAnimationFrame"in window?e.requestAnimationFrame(b):e.setTimeout(b,p.delay)}()},a.prototype.checkFontFaces=function(n){var r=this;t.fonts.forEach(function(t){t.family.toLowerCase()===r.fontFamily.toLowerCase()&&t.load().then(function(){r.options.success(),e.clearTimeout(n)})})},a.prototype.init=function(n,r){var i;for(var o in s)r.hasOwnProperty(o)||(r[o]=s[o]);this.options=r,this.fontFamily=n,!r.glyphs&&"fonts"in t?(r.timeout&&(i=e.setTimeout(function(){r.error()},r.timeout)),this.checkFontFaces(i)):this.load()};var f=function(e,t){var n=new a;return n.init(e,t),n};e.FontFaceOnload=f})(this,this.document);
+/*! fontfaceonload - v0.1.4 - 2015-01-23
+ * https://github.com/zachleat/fontfaceonload
+ * Copyright (c) 2015 Zach Leatherman (@zachleat)
+ * MIT License */(function(e,t){"use strict";var n="AxmTYklsjo190QW",r="sans-serif",i="serif",s={tolerance:2,delay:100,glyphs:"",success:function(){},error:function(){},timeout:5e3,weight:"400",style:"normal"},o=["display:block","position:absolute","top:-999px","left:-999px","font-size:48px","width:auto","height:auto","line-height:normal","margin:0","padding:0","font-variant:normal","white-space:nowrap"],u='<div style="%s">'+n+"</div>",a=function(){this.fontFamily="",this.appended=!1,this.serif=undefined,this.sansSerif=undefined,this.parent=undefined,this.options={}};a.prototype.getMeasurements=function(){return{sansSerif:{width:this.sansSerif.offsetWidth,height:this.sansSerif.offsetHeight},serif:{width:this.serif.offsetWidth,height:this.serif.offsetHeight}}},a.prototype.load=function(){function v(e){return o.concat(["font-weight:"+p.weight,"font-style:"+p.style]).concat("font-family:"+e).join(";")}function y(e,t,n){return Math.abs(e.width-t.offsetWidth)>n||Math.abs(e.height-t.offsetHeight)>n}function b(){return(new Date).getTime()-n.getTime()>p.timeout}var n=new Date,s=this,a=s.serif,f=s.sansSerif,l=s.parent,c=s.appended,h,p=this.options,d=p.reference,m=u.replace(/\%s/,v(r)),g=u.replace(/\%s/,v(i));l||(l=s.parent=t.createElement("div")),l.innerHTML=m+g,f=s.sansSerif=l.firstChild,a=s.serif=f.nextSibling,p.glyphs&&(f.innerHTML+=p.glyphs,a.innerHTML+=p.glyphs),function w(){d||(d=t.body),!c&&d&&(d.appendChild(l),c=s.appended=!0,h=s.getMeasurements(),f.style.fontFamily=s.fontFamily+", "+r,a.style.fontFamily=s.fontFamily+", "+i),c&&h&&(y(h.sansSerif,f,p.tolerance)||y(h.serif,a,p.tolerance))?p.success():b()?p.error():!c&&"requestAnimationFrame"in window?e.requestAnimationFrame(w):e.setTimeout(w,p.delay)}()},a.prototype.checkFontFaces=function(n){var r=this;t.fonts.forEach(function(t){t.family.toLowerCase()===r.fontFamily.toLowerCase()&&t.weight===""+r.options.weight&&t.style===r.options.style&&t.load().then(function(){r.options.success(),e.clearTimeout(n)})})},a.prototype.init=function(n,r){var i;for(var o in s)r.hasOwnProperty(o)||(r[o]=s[o]);this.options=r,this.fontFamily=n,!r.glyphs&&"fonts"in t?(r.timeout&&(i=e.setTimeout(function(){r.error()},r.timeout)),this.checkFontFaces(i)):this.load()};var f=function(e,t){var n=new a;return n.init(e,t),n};e.FontFaceOnload=f})(this,this.document);
 
 	var docEl = doc.documentElement;
 
@@ -45,18 +44,26 @@ var ZL = {
 			docEl.className += " lato-loaded";
 
 			var counter = 0;
-			var options = {
-				success: function() {
-					counter++;
-					if( counter === 3 ) {
-						docEl.className += " lato-b-loaded";
-					}
+			var success = function() {
+				counter++;
+				if( counter === 3 ) {
+					docEl.className += " lato-b-loaded";
 				}
-			}
+			};
 
-			FontFaceOnload( "LatoBold", options );
-			FontFaceOnload( "LatoItalic", options );
-			FontFaceOnload( "LatoBoldItalic", options );
+			FontFaceOnload( "LatoBold", {
+				weight: 700,
+				success: success
+			});
+			FontFaceOnload( "LatoItalic", {
+				style: 'italic',
+				success: success
+			});
+			FontFaceOnload( "LatoBoldItalic", {
+				weight: 700,
+				style: 'italic',
+				success: success
+			});
 		}
 	});
 
