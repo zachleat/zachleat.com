1edd5fcf5cf142f25e981ffce2dda9a1d40d580f
Zach Leatherman <zachleatherman@gmail.com>
2015-12-17 08:42:55 -0600

    incremental change, didn’t bump version.

--- web/_includes/async.js
+++ web/_includes/async.js
@@ -1,4 +1,4 @@
-/*! zachleat.com - v0.3.1 - 2015-12-16
+/*! zachleat.com - v0.3.1 - 2015-12-17
 * Copyright (c) 2015 Zach Leatherman; MIT License */
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
 (function( w ){
--- web/_includes/initial.js
+++ web/_includes/initial.js
@@ -1,4 +1,4 @@
-/*! zachleat.com - v0.3.1 - 2015-12-16
+/*! zachleat.com - v0.3.1 - 2015-12-17
 * Copyright (c) 2015 Zach Leatherman; MIT License */
 var ZL = {
 	getDistFolder: function() {
