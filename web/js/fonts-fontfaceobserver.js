5369a74e3edf32e861d5b0bfdbf0177dbc3e596f
Zach Leatherman <zachleatherman@gmail.com>
2016-01-06 08:00:28 -0600

    Updates to font code

--- web/js/fonts-fontfaceobserver.js
+++ web/js/fonts-fontfaceobserver.js
@@ -1,40 +1,38 @@
 /* Fonts */
 ;(function( doc ) {
 	// IE9+
-	if( !( 'geolocation' in navigator ) ) {
+	if( !( 'geolocation' in navigator ) ||
+		sessionStorage.latoStageOne && sessionStorage.latoStageTwo ) {
 		return;
 	}
 
 	var docEl = doc.documentElement;
+	var observer = new FontFaceObserver( "LatoSubset", {
+		weight: 400
+	});
 
-	if( !sessionStorage.latoStageOne || !sessionStorage.latoStageTwo ) {
-		var observer = new FontFaceObserver( "LatoSubset", {
-			weight: 400
+	observer.check().then(function () {
+		docEl.className += " lato-loaded";
+		sessionStorage.latoStageOne = true;
+
+		Promise.all([
+			(new FontFaceObserver( "Lato", {
+				weight: 400
+			})).check(),
+			(new FontFaceObserver( "LatoBold", {
+				weight: 700
+			})).check(),
+			(new FontFaceObserver( "LatoItalic", {
+				style: "italic"
+			})).check(),
+			(new FontFaceObserver( "LatoBoldItalic", {
+				weight: 700,
+				style: "italic"
+			})).check(),
+		]).then(function () {
+			docEl.className += " lato-b-loaded";
+			sessionStorage.latoStageTwo = true;
 		});
-
-		observer.check().then(function () {
-			docEl.className += " lato-loaded";
-			sessionStorage.latoStageOne = true;
-
-			Promise.all([
-				(new FontFaceObserver( "Lato", {
-					weight: 400
-				})).check(),
-				(new FontFaceObserver( "LatoBold", {
-					weight: 700
-				})).check(),
-				(new FontFaceObserver( "LatoItalic", {
-					style: "italic"
-				})).check(),
-				(new FontFaceObserver( "LatoBoldItalic", {
-					weight: 700,
-					style: "italic"
-				})).check(),
-			]).then(function () {
-				docEl.className += " lato-b-loaded";
-				sessionStorage.latoStageTwo = true;
-			});
-		});
-	}
+	});
 
 })( document );
