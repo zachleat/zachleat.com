9cb85f80b8edeef3394c8df8aa82990e1441e0f4
Zach Leatherman <zachleatherman@gmail.com>
2016-03-31 23:26:16 -0500

    v0.3.8

--- web/_includes/initial.js
+++ web/_includes/initial.js
@@ -1,4 +1,4 @@
-/*! zachleat.com - v0.3.7 - 2016-03-15
+/*! zachleat.com - v0.3.8 - 2016-03-31
 * Copyright (c) 2016 Zach Leatherman; MIT License */
 var ZL = {
 	getDistFolder: function() {
@@ -23,13 +23,6 @@ var ZL = {
 	// gradient inference
 	if( 'matchMedia' in window ) {
 		classes.push( "has-gradient" );
-	}
-
-	// Fonts
-	if( sessionStorage.latoStageOne && sessionStorage.latoStageTwo ) {
-		// Don’t push this class, lato-loaded is just for the subset font (we don’t need it)
-		// classes.push( "lato-loaded" );
-		classes.push( "lato-b-loaded" );
 	}
 
 	document.documentElement.className += " " + classes.join( " " );
@@ -242,47 +235,57 @@ var ZL = {
 
 ;(function( doc ) {
 	// IE9+
-	if( !( 'geolocation' in navigator ) ||
-		!( "keys" in Object ) ||
-		sessionStorage.latoStageOne && sessionStorage.latoStageTwo ) {
+	if( !( 'geolocation' in navigator ) || !( "keys" in Object ) ) {
 		return;
 	}
 
 	var docEl = doc.documentElement;
+	var timer = new Date();
+	var WAIT_TIME = 3000;
 
-	FontFaceOnload( "LatoSubset", {
+	function firstStageSuccess() {
+		docEl.className += " webfont-stage-1";
+
+		var stage2 = {
+			ComicNeueBold: {
+				weight: 700
+			},
+			ComicNeueItalic: {
+				weight: 300,
+				style: "italic"
+			},
+			ComicNeueBoldItalic: {
+				weight: 700,
+				style: "italic"
+			}
+		};
+		var counter = 0;
+		var param;
+		var numberOfFonts = Object.keys( stage2 ).length;
+		var secondStageSuccess = function() {
+			counter++;
+			if( counter === numberOfFonts ) {
+				docEl.className += " webfont-stage-2";
+				sessionStorage.webfontStageTwo = true;
+			}
+		};
+
+		for( var name in stage2 ) {
+			param = stage2[ name ];
+			param.success = secondStageSuccess;
+			FontFaceOnload( name, param );
+		}
+	}
+
+	FontFaceOnload( "ComicNeue", {
+		weight: 300,
 		success: function() {
-			docEl.className += " lato-loaded";
-			sessionStorage.latoStageOne = true;
-
-			var stage2 = {
-				Lato: {},
-				LatoBold: {
-					weight: 700
-				},
-				LatoItalic: {
-					style: "italic"
-				},
-				LatoBoldItalic: {
-					weight: 700,
-					style: "italic"
-				}
-			};
-			var counter = 0;
-			var param;
-			var numberOfFonts = Object.keys( stage2 ).length;
-			var success = function() {
-				counter++;
-				if( counter === numberOfFonts ) {
-					docEl.className += " lato-b-loaded";
-					sessionStorage.latoStageTwo = true;
-				}
-			};
-
-			for( var name in stage2 ) {
-				param = stage2[ name ];
-				param.success = success;
-				FontFaceOnload( name, param );
+			var offset = new Date() - timer;
+			var left = WAIT_TIME - offset;
+			if( left > 0 ) {
+				setTimeout( firstStageSuccess, left );
+			} else {
+				firstStageSuccess();
 			}
 		}
 	});
