9b5baad100cffab1092c90ada89c77510a9f3e90
Zach Leatherman <zachleatherman@gmail.com>
2016-04-19 21:00:32 -0500

    test2.zachleat.com

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,7 +247,7 @@ module.exports = function(grunt) {
 			},
 			// TODO https://github.com/shama/grunt-beep
 			upload: {
-				command: 'echo "Note: Requires an \'zachleat-test\' host in .ssh/config"; rsync --archive --verbose --stats --compress --rsh=ssh ./_site/ zachleat-test:/home/public/<%= config.root %>',
+				command: 'echo "Note: Requires an \'zachleat-test2\' host in .ssh/config"; rsync --archive --verbose --stats --compress --rsh=ssh ./_site/ zachleat-test2:/home/public/<%= config.root %>',
 				options: {
 					maxBuffer: 1024 * 1024 * 64,
 					execOptions: {
