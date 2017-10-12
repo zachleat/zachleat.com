ec6f906fce45402bd15f4323ed3e60c344f547bd
Zach Leatherman <zachleatherman@gmail.com>
2017-10-11 22:38:34 -0500

    Async polyfill font load path was using the subset (oops).

--- web/js/fonts-polyfill-fontfaceonload.js
+++ web/js/fonts-polyfill-fontfaceonload.js
@@ -6,43 +6,36 @@
 	}
 
 	var docEl = doc.documentElement;
-	FontFaceOnload( "LatoSubset", {
-		success: function() {
-			docEl.className += " webfont-stage-1";
-			sessionStorage.webfontStageOne = true;
-
-			var stage2 = {
-				Lato: {},
-				LatoBold: {
-					weight: 700
-				},
-				LatoItalic: {
-					style: "italic"
-				},
-				LatoBoldItalic: {
-					weight: 700,
-					style: "italic"
-				}
-			};
-			var counter = 0;
-			var param;
-			var numberOfFonts = Object.keys( stage2 ).length;
-			var success = function() {
-				counter++;
-				if( counter === numberOfFonts ) {
-					// Currently emulating `font-display: optional`
-					// Uncomment to switch to `font-display: swap`
-					// docEl.className += " webfont-stage-2";
-					sessionStorage.webfontStageTwo = true;
-				}
-			};
-
-			for( var name in stage2 ) {
-				param = stage2[ name ];
-				param.success = success;
-				FontFaceOnload( name, param );
-			}
+	var stage2 = {
+		Lato: {},
+		LatoBold: {
+			weight: 700
+		},
+		LatoItalic: {
+			style: "italic"
+		},
+		LatoBoldItalic: {
+			weight: 700,
+			style: "italic"
 		}
-	});
+	};
+	var counter = 0;
+	var param;
+	var numberOfFonts = Object.keys( stage2 ).length;
+	var success = function() {
+		counter++;
+
+		if( counter === numberOfFonts ) {
+			// Currently emulating `font-display: optional`, don’t add any classes
+			sessionStorage.webfontStageOne = true;
+			sessionStorage.webfontStageTwo = true;
+		}
+	};
+
+	for( var name in stage2 ) {
+		param = stage2[ name ];
+		param.success = success;
+		FontFaceOnload( name, param );
+	}
 
 })( document );
