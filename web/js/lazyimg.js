d3ec315d7e117c9907704f1a804f1145594c272d
Zach Leatherman <zachleatherman@gmail.com>
2019-06-29 13:08:10 -0500

    Handle lazy loading image errors

--- web/js/lazyimg.js
+++ web/js/lazyimg.js
@@ -7,6 +7,10 @@ if( typeof IntersectionObserver !== "undefined" && "forEach" in NodeList.prototy
 		changes.forEach(function(change) {
 			if(change.isIntersecting) {
 				change.target.setAttribute("src", change.target.getAttribute("data-src"));
+				change.target.addEventListener("error", function() {
+					change.target.parentNode.classList.add("img-error");
+					// change.target.parentNode.removeChild(change.target);
+				}, false);
 				observer.unobserve(change.target);
 			}
 		});
