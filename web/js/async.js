a98b1bba2e244b6177ad0a468ea6c87965f39ab3
Zach Leatherman <zachleatherman@gmail.com>
2020-07-10 22:34:23 -0500

    Fixes deprecated post treatment slowing down the archives page.

--- web/js/async.js
+++ web/js/async.js
@@ -21,25 +21,4 @@
 	var banner = ' w3c-b ' + [ 'ud', 'wd', 'cr', 'pr', 'r', 'wgn', 'ign', 'ed' ][ Math.floor( Math.random() * 8 ) ];
 	document.documentElement.className += banner;
 	document.documentElement.setAttribute( "data-w3c-banner-label", "Non-canonical web standards fan fiction" );
-
-	// Filter Posts Menu
-	var filter = doc.getElementById( 'post-filter' );
-	var posts = doc.getElementById( 'main-posts-list' );
-
-	function updateFilter() {
-		if( !filter.checked ) {
-			posts.classList.remove( "all" );
-			posts.classList.add( "deprecated" );
-		} else {
-			posts.classList.remove( "deprecated" );
-			posts.classList.add( "all" );
-		}
-	}
-
-	if( filter && posts && ( "classList" in posts ) ) {
-		posts.classList.add( "enhanced" );
-
-		updateFilter();
-		filter.addEventListener( 'change', updateFilter, false );
-	}
 })( document );
