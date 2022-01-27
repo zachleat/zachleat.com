6afa36f8de692a198f493c956cad075a8d233fb4
Zach Leatherman <zachleatherman@gmail.com>
2022-01-27 13:56:47 -0600

    bustin

--- _includes/imagePlugin.js
+++ _includes/imagePlugin.js
@@ -71,7 +71,7 @@ function getServiceCacheBuster() {
     // return `_${d.getFullYear()}${pad(d.getMonth()+1)}${pad(d.getDate())}`;
 
     // Weekly
-    return `___${d.getFullYear()}${pad(d.getMonth()+1)}_${d.getDate() % 7}`;
+    return `_${d.getFullYear()}${pad(d.getMonth()+1)}_${d.getDate() % 7}`;
   }
 
   // return a throwaway constant cachebuster ref so that we don’t accidentally request production urls during local dev before they’re available online.
