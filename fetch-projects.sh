98e04beedaa8706c301688184ec0d07209797be0
Zach Leatherman <zachleatherman@gmail.com>
2022-02-04 10:39:28 -0600

    Degit straight to output directory

--- fetch-projects.sh
+++ fetch-projects.sh
@@ -1,15 +1,16 @@
-# Entries must be added to .addPassthroughCopy and .gitignore
-rm -rf unicode-range-interchange
-npx degit zachleat/unicode-range-interchange unicode-range-interchange
+mkdir -p _site
 
-rm -rf alarmd2
-npx degit zachleat/ALARMd alarmd2
+rm -rf _site/unicode-range-interchange
+npx degit zachleat/unicode-range-interchange _site/unicode-range-interchange
 
-rm -rf bigtext
-npx degit zachleat/bigtext bigtext
+rm -rf _site/alarmd2
+npx degit zachleat/ALARMd _site/alarmd2
 
-rm -rf presentations/toolordie/
-npx degit zachleat/ToolOrDie presentations/toolordie
+rm -rf _site/bigtext
+npx degit zachleat/bigtext _site/bigtext
 
-rm -rf web-fonts/demos/
-npx degit zachleat/web-font-loading-recipes web-fonts/demos
+rm -rf _site/presentations/toolordie
+npx degit zachleat/ToolOrDie _site/presentations/toolordie
+
+rm -rf _site/web-fonts/demos
+npx degit zachleat/web-font-loading-recipes _site/web-fonts/demos
