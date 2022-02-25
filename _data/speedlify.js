b3a6c623e3149fec4b69220d6f9abbe2f72c4d1d
Zach Leatherman <zachleatherman@gmail.com>
2022-02-25 16:52:30 -0600

    Upgrade to use Eleventy Fetch

--- _data/speedlify.js
+++ _data/speedlify.js
@@ -1,8 +1,8 @@
-const CacheAsset = require("@11ty/eleventy-cache-assets");
+const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
 	let url = "https://www.speedlify.dev/api/urls.json";
-	let json = await CacheAsset(url, {
+	let json = await EleventyFetch(url, {
 		duration: "1d",
 		type: "json",
 	});
