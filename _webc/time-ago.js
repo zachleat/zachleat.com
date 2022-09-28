e50a29218bf5601ea4221890c337ad50d40b953d
Zach Leatherman <zachleatherman@gmail.com>
2022-09-28 11:01:00 -0500

    Tabs

--- _webc/time-ago.js
+++ _webc/time-ago.js
@@ -1,28 +1,28 @@
 class TimeAgo extends HTMLElement {
 	static tagName = "time-ago";
 
-  getDate() {
-    let timestamp = this.getAttribute("timestamp");
-    if(timestamp) {
-      return (new Date()).setTime(timestamp);
-    }
+	getDate() {
+		let timestamp = this.getAttribute("timestamp");
+		if(timestamp) {
+			return (new Date()).setTime(timestamp);
+		}
 
-    return Date.parse(this.getAttribute("date"));
-  }
+		return Date.parse(this.getAttribute("date"));
+	}
 
-  connectedCallback() {
-    let locale = this.getAttribute("locale") || "en";
-    let rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
-    let date = this.getDate();
-    if(!date) {
-      return;
-    }
+	connectedCallback() {
+		let locale = this.getAttribute("locale") || "en";
+		let rtf = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
+		let date = this.getDate();
+		if(!date) {
+			return;
+		}
 
-    let yearsDiff = Math.floor((date - Date.now())/(1000 * 60 * 60 * 24 * 365));
+		let yearsDiff = Math.floor((date - Date.now())/(1000 * 60 * 60 * 24 * 365));
 		this.innerText = rtf.format(yearsDiff, "year");
-  }
+	}
 }
 
 if(("customElements" in window) && Date.parse && ("Intl" in window) && Intl.RelativeTimeFormat) {
-  window.customElements.define(TimeAgo.tagName, TimeAgo);
+	window.customElements.define(TimeAgo.tagName, TimeAgo);
 }
