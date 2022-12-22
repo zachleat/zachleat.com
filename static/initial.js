0db1f6baa3904cd40112863b83efc5ca9d3c07aa
Zach Leatherman <zachleatherman@gmail.com>
2022-12-22 14:51:34 -0600

    Gets rid of grunt, everything is Eleventy now

--- static/initial.js
+++ static/initial.js
@@ -1,5 +1,3 @@
-/*! zachleat.com - v2.6.1 - 2022-12-22
-* Copyright (c) 2022 Zach Leatherman; @license MIT License */
 ;(function( doc ) {
 	// IE9+
 	if( !( 'geolocation' in navigator ) ) {
--- web/js/twitter-api.js
+++
@@ -1,9 +0,0 @@
-// Twitter Follow Button
-;(function( doc ) {
-	// IE9+ and not on the home page.
-	if( !( 'geolocation' in navigator ) || !doc.querySelector( 'meta[name="template"][content="page"]' ) ) {
-		return;
-	}
-
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-})( document );
