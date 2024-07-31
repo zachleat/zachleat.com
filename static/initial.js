c76d741ab4ca2b5c536157c335f908797c375fe8
Zach Leatherman <zachleat@users.noreply.github.com>
2024-07-31 15:48:36 -0500

    Upgrade to Eleventy 3.0 beta

--- static/initial.js
+++ static/initial.js
@@ -1,6 +1,6 @@
 // Home-made analytics
 if(( 'sendBeacon' in navigator ) && location.hostname === "www.zachleat.com") {
-	// Elizabeacon, FaunaDB=bacled
+	// Elizabeacon, FaunaDB
 	navigator.sendBeacon("https://subtle-yeot-bd8178.netlify.app/", JSON.stringify({
 		path: location.pathname
 	}));
