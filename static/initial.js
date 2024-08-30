9464a1f23bd1053f2a25d2f391e94334979187cb
Zach Leatherman <zachleat@users.noreply.github.com>
2024-08-30 10:44:05 -0500

    FaunaDB failed

--- static/initial.js
+++ static/initial.js
@@ -1,7 +1,7 @@
 // Home-made analytics
 if(( 'sendBeacon' in navigator ) && location.hostname === "www.zachleat.com") {
 	// Elizabeacon, FaunaDB
-	navigator.sendBeacon("https://subtle-yeot-bd8178.netlify.app/", JSON.stringify({
-		path: location.pathname
-	}));
+	// navigator.sendBeacon("https://subtle-yeot-bd8178.netlify.app/", JSON.stringify({
+	// 	path: location.pathname
+	// }));
 }
