7c4f8d3e92c92e6111055f14973e60ea58f2ac9c
Zach Leatherman <zachleat@users.noreply.github.com>
2025-02-28 08:58:46 -0600

    Bumping cache buster again

--- _11ty/imagePlugin.js
+++ _11ty/imagePlugin.js
@@ -3,7 +3,7 @@ import Image, { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { getImageColors } from "@11ty/image-color";
 
 const SIZES_INLINE = "(min-width: 75em) 44.5625em, (min-width: 61.25em) 40.6875em, (min-width: 41.25em) 36.8125em, 96vw";
-export const CACHEBUSTER = "20250228";
+export const CACHEBUSTER = "20250229";
 
 function getCryptoHash(src) {
 		let hash = createHash("sha1");
@@ -94,6 +94,7 @@ function opengraphImageHtmlWithClass(targetUrl, alt = "", cls = "") {
 			}
 
 			if(process.env.PRODUCTION_BUILD) {
+				// Can probably remove CACHEBUSTER in about a week from 2025-02-28
 				return `${fullUrl}${size}/${format}/_${CACHEBUSTER}/`;
 			}
 
