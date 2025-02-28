b23cad6dde51ad89e4e058a0e428d36f5b0a4878
Zach Leatherman <zachleat@users.noreply.github.com>
2025-02-28 13:52:15 -0600

    Add rejectionhandled

--- run-build.js
+++ run-build.js
@@ -5,6 +5,7 @@ process.env.ELEVENTY_FEATURES="webmentions";
 
 process.on('uncaughtException', (e) => console.error('uncaughtException', e))
 process.on('unhandledRejection', (e) => console.error('unhandledRejection', e))
+process.on("rejectionHandled", (e) => console.error('rejectionHandled', e));
 
 let elev = new Eleventy();
 await elev.write();
