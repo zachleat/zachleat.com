20991a0a8bf23ebdb922de033f8b6321e00f212b
Zach Leatherman <zachleat@users.noreply.github.com>
2025-10-22 07:45:46 -0500

    Show min count on analytics

--- _11ty/fetch-analytics/goatcounter.js
+++ _11ty/fetch-analytics/goatcounter.js
@@ -8,7 +8,8 @@ async function fetchData() {
 		}
 
 		// The results are sorted by count descending
-		let json = await EleventyFetch("https://zachleat.goatcounter.com/api/v0/stats/hits?start=2024-08-30&limit=100&daily=true", {
+		// Warning: the last site in the 100-paths max dataset has about ~80 hits so any site with less than that won’t show up (minimum is logged below)
+		let json = await EleventyFetch("https://zachleat.goatcounter.com/api/v0/stats/hits?start=2024-08-30&limit=100", {
 			type: "json",
 			duration: process.env.ELEVENTY_RUN_MODE === "build" ? "0s" : "1h",
 			directory: ".cache/eleventy-fetch/",
@@ -22,6 +23,7 @@ async function fetchData() {
 
 		let ret = {};
 		let total = 0;
+		let minimum = 99999;
 		json.hits.forEach(entry => {
 			total += entry.count;
 
@@ -30,9 +32,12 @@ async function fetchData() {
 				count: entry.count,
 				path: urlPath,
 			}
+			if(entry.count < minimum) {
+				minimum = entry.count;
+			}
 		});
 
-		console.log( "[goatcounter] Found", json.hits.length, "URLs (max: 100) with", total, "Pageviews" );
+		console.log( "[goatcounter] Found", json.hits.length, "URLs (max: 100) with", total, "views. Smallest path had", minimum, "views" );
 		return ret;
 	} catch(e) {
 		if(process.env.NODE_ENV === "production") {
