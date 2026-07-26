d5950dd4b7f4f01a8a7ebb5bc67f9a8c4754e9b9
Zach Leatherman <zachleat@users.noreply.github.com>
2026-07-26 14:44:52 -0500

    Adds projects page

---
+++ projects.11tydata.js
@@ -0,0 +1,30 @@
+import Fetch from "@11ty/eleventy-fetch";
+
+export const minimumDownloads = 1000;
+export const minimumStars = 200;
+
+let report = await Fetch("https://zachleat.github.io/is-this-still-being-maintained/report.json", {
+	type: "json",
+	duration: "30m",
+});
+
+export const packages = report.projects.sort((a, b) => {
+	return b.score - a.score;
+});
+
+let totalCounts = {
+	prs: { open: 0, closed: 0 },
+	issues: { open: 0, closed: 0 },
+	stars: 0,
+	downloads: 0,
+};
+for(let pkg of report.projects) {
+	totalCounts.prs.open += pkg.openPRs;
+	totalCounts.prs.closed += pkg.mergedPRs + pkg.closedPRs;
+	totalCounts.issues.open += pkg.openIssues;
+	totalCounts.issues.closed += pkg.closedIssues;
+	totalCounts.stars += pkg.stars;
+	totalCounts.downloads += pkg.downloads;
+}
+
+export const totals = totalCounts;
