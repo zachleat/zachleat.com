acf5fffb32115c8848641b441f2509843ac9a3b8
Zach Leatherman <zachleat@users.noreply.github.com>
2026-07-26 15:42:20 -0500

    Add tablesaw

--- projects.11tydata.js
+++ projects.11tydata.js
@@ -5,7 +5,7 @@ export const minimumStars = 200;
 
 let report = await Fetch("https://zachleat.github.io/is-this-still-being-maintained/report.json", {
 	type: "json",
-	duration: "5m",
+	duration: "30m",
 });
 
 export const packages = report.projects.sort((a, b) => {
