853acc9be77d169ef7a24fe138e1bd2b16eb9ff8
Zach Leatherman <zachleat@users.noreply.github.com>
2026-07-26 19:36:10 -0500

    Adds repos that have workspaces, clean the output up a bit

--- projects.11tydata.js
+++ projects.11tydata.js
@@ -17,14 +17,24 @@ let totalCounts = {
 	issues: { open: 0, closed: 0 },
 	stars: 0,
 	downloads: 0,
+	publishes: 0
 };
+let reposSeen = new Set();
 for(let pkg of report.projects) {
+	totalCounts.publishes += pkg.publishCount;
+	totalCounts.downloads += pkg.downloads;
+
+	// Workspace counts would duplicate if we didn’t check
+	if(reposSeen.has(pkg.url)) {
+		continue;
+	}
+	reposSeen.add(pkg.url);
+
 	totalCounts.prs.open += pkg.openPRs;
 	totalCounts.prs.closed += pkg.mergedPRs + pkg.closedPRs;
 	totalCounts.issues.open += pkg.openIssues;
 	totalCounts.issues.closed += pkg.closedIssues;
 	totalCounts.stars += pkg.stars;
-	totalCounts.downloads += pkg.downloads;
 }
 
 export const totals = totalCounts;
